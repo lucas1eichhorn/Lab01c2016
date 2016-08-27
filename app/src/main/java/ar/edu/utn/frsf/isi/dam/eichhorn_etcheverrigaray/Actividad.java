422da972c5ce4539c7f2acb5c4091810d9006bd9
Lucas <lucas1eichhorn@gmail.com>
2016-08-26 21:58:49 -0300

    commit inicial

---
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/eichhorn_etcheverrigaray/Actividad.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.frsf.isi.dam.eichhorn_etcheverrigaray;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Actividad extends AppCompatActivity {
+Button bto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_principal);
+
+
+
+
+
+    }
+}
